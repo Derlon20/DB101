e2997b574215c836f0e798e94e738406f7833b6d
Derlon20 <91274138+Derlon20@users.noreply.github.com>
2024-01-03 01:48:55 +0200

    Update query.sql

--- db_lab3_Nikitin/query.sql
+++ db_lab3_Nikitin/query.sql
@@ -1,24 +1,21 @@
 -- 1.Вивести жанри тих ігор, в які можна грати самому
-SELECT DISTINCT(d.domain)
-FROM domains d
-JOIN domain_of_game dog ON dog.domain_id = d.domain_id
-JOIN game g ON g.game_id = dog.game_id
-WHERE g.min_player = 1;
+select distinct(domain) from domains
+join domain_of_game  on domain_of_game.domain_id = domains.domain_id
+join game on game.game_id = domain_of_game.game_id
+where game.min_player = 1;
 
 -- 2.Вивести найдавнішу гру з жанром 'Стратегія'
-SELECT g.Name
-FROM game g
-JOIN domain_of_game dog ON g.Game_id = dog.Game_id
-JOIN domains d ON dog.domain_id = d.domain_id
-WHERE d.domain = 'Strategy Games'
-ORDER BY g.Year
-LIMIT 1;
+select name from game 
+join domain_of_game on game.game_id = domain_of_game.game_id
+join domains on domain_of_game.domain_id = domains.domain_id
+where domains.domain = 'Strategy Games'
+order by game.year
+limit 1;
 
 -- 3. Вивести найдовший час серед ігор з більш ніж одним жанром
-SELECT g.time
-FROM game g
-JOIN domain_of_game dog ON g.game_id = dog.game_id
-GROUP BY g.game_id
-HAVING COUNT(DISTINCT dog.domain_id) > 1
-ORDER BY g.Time desc
-LIMIT 1;
+select time from game
+join domain_of_game on game.game_id = domain_of_game.game_id
+group by game.game_id
+having count(distinct(domain_of_game.domain_id)) > 1
+order by game.time desc
+limit 1;
