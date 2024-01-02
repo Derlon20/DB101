bc9f6b6d14579c694e8c5f6e51ebefb50da5d945
Derlon20 <91274138+Derlon20@users.noreply.github.com>
2024-01-03 01:57:16 +0200

    Update query.sql

--- db_lab4_Nikitin/query.sql
+++ db_lab4_Nikitin/query.sql
@@ -1,19 +1,17 @@
 --1. Час кожної гри
-SELECT g.name, g.time
-FROM Game g
-ORDER BY Time DESC;
+select name, time from game
+order by time desc;
 
 --2. Розподіл ігор за жанрами
-SELECT D.domain, COUNT(DG.Game_id) AS GameCount
-FROM Domains D
-LEFT JOIN Domain_of_Game DG ON D.domain_id = DG.domain_id
-GROUP BY D.domain
-ORDER BY GameCount DESC;
+select domain, count(domain_of_game.Game_id) as GameCount
+from Domains
+left join domain_of_game on domains.domain_id = domain_of_game.domain_id
+group by domains.domain
+order by GameCount desc;
 
 --3. Середній час ігор кожного жанру
-SELECT D.domain, AVG(G.Time) AS AverageTime
-FROM Domains D
-JOIN Domain_of_Game DG ON D.domain_id = DG.domain_id
-JOIN Game G ON DG.Game_id = G.Game_id
-GROUP BY D.domain
-ORDER BY AverageTime DESC;
+select domain, avg(game.time) as AverageTime from domains 
+join domain_of_game on domains.domain_id = domain_of_game.domain_id
+join game on domain_of_game.game_id = game.game_id
+group by domains.domain
+order by AverageTime desc;
